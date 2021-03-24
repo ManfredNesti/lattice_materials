7dbdb73a060d11df252cc55c93fab95700799a7e
Manfred Nesti <manfred.nesti@mail.polimi.it>
2021-03-24 16:56:07 +0000

    case1_non_liner library and script

---
+++ case1_non_linear/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir results_case1_non_linear;
+STR=$(date '+%Y-%m-%d_%H-%M-%S');
+mkdir results_case1_non_linear/$STR;
+FreeFem++ -ne main.edp 2>&1 | tee results_case1_non_linear/$STR/output.txt;
+wait;
+mv results_case1_non_linear/*.vtk results_case1_non_linear/$STR;
+rm ipopt.out;
+echo "*** SCRIPT NORMAL END ***";
