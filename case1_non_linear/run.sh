c6d64fc3c357aa4fef2b4832501ee3f09f3d934e
Manfred Nesti <manfred.nesti@mail.polimi.it>
2021-05-03 09:02:09 +0000

    Trials

--- case1_non_linear/run.sh
+++ case1_non_linear/run.sh
@@ -2,7 +2,7 @@
 mkdir results_case1_non_linear;
 STR=$(date '+%Y-%m-%d_%H-%M-%S');
 mkdir results_case1_non_linear/$STR;
-FreeFem++ -ne main.edp 2>&1 | tee results_case1_non_linear/$STR/output.txt;
+FreeFem++ main.edp 2>&1 | tee results_case1_non_linear/$STR/output.txt;
 wait;
 mv results_case1_non_linear/*.vtk results_case1_non_linear/$STR;
 rm ipopt.out;
