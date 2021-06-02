57ebde69d5364c6bf2bec084eb9011b129a4d206
Manfred Nesti <manfred.nesti@mail.polimi.it>
2021-06-02 10:09:31 +0000

    Remove sigIN and control beta/eta from params

--- non_linear/run.sh
+++ non_linear/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-mkdir results_case1_non_linear;
+mkdir results;
 STR=$(date '+%Y-%m-%d_%H-%M-%S');
-mkdir results_case1_non_linear/$STR;
-mkdir results_case1_non_linear/$STR/code/;
-FreeFem++ main.edp 2>&1 | tee results_case1_non_linear/$STR/output.txt;
+mkdir results/$STR;
+mkdir results/$STR/code/;
+FreeFem++ main.edp 2>&1 | tee results/$STR/output.txt;
 wait;
-# for f in results_case1_non_linear/*.ps
+# for f in results/*.ps
 # do
-#   ps2png results_case1_non_linear/$f $f.png
+#   ps2png results/$f $f.png
 # done
-# mv results_case1_non_linear/*.png results_case1_non_linear/$STR;
-cp *.edp results_case1_non_linear/$STR/code/;
-sed -n '/savemesh/,$p' results_case1_non_linear/$STR/output.txt > results_case1_non_linear/$STR/output_clean.txt;
-mv results_case1_non_linear/*.vtk results_case1_non_linear/$STR;
-mv results_case1_non_linear/*.ps results_case1_non_linear/$STR;
-mv results_case1_non_linear/*.msh results_case1_non_linear/$STR;
-mv results_case1_non_linear/*.txt results_case1_non_linear/$STR;
+# mv results/*.png results/$STR;
+cp *.edp results/$STR/code/;
+sed -n '/savemesh/,$p' results/$STR/output.txt > results/$STR/output_clean.txt;
+mv results/*.vtk results/$STR;
+mv results/*.ps results/$STR;
+mv results/*.msh results/$STR;
+mv results/*.txt results/$STR;
 
 rm ipopt.out;
 echo "*** SCRIPT NORMAL END ***";
