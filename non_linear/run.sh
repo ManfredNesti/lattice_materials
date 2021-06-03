8fce0886ff41e0558cda2c3c685f336e84d6dc6e
Manfred Nesti <manfred.nesti@mail.polimi.it>
2021-06-03 15:37:57 +0000

    Checknu

--- non_linear/run.sh
+++ non_linear/run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+
+echo "----------------------------------------------------";
+echo "---      Lattice material optimization with      ---";
+echo "---            non linear elastic law            ---";
+echo "---                                              ---";
+echo "--- Teresa Babini (teresa.babini@mail.polimi.it) ---";
+echo "--- Manfred Nesti (manfred.nesti@mail.polimi.it) ---";
+echo "----------------------------------------------------";
+
 mkdir results;
-STR=$(date '+%Y-%m-%d_%H-%M-%S');
+STR = $(date '+%Y-%m-%d_%H-%M-%S');
 mkdir results/$STR;
 mkdir results/$STR/code/;
 FreeFem++ main.edp 2>&1 | tee results/$STR/output.txt;
 wait;
-# for f in results/*.ps
-# do
-#   ps2png results/$f $f.png
-# done
-# mv results/*.png results/$STR;
 cp *.edp results/$STR/code/;
 sed -n '/savemesh/,$p' results/$STR/output.txt > results/$STR/output_clean.txt;
 mv results/*.vtk results/$STR;
 mv results/*.ps results/$STR;
 mv results/*.msh results/$STR;
 mv results/*.txt results/$STR;
-
 rm ipopt.out;
-echo "*** SCRIPT NORMAL END ***";
+
+echo "--- Script ended ---";
